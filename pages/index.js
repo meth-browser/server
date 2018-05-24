a670ea9c8995f45ec7dd4012fcdec74252ea6942
Ramesh Nair <ram@hiddentao.com>
2018-05-24 15:48:19 +0100

    add skeleton index page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const githubRepoUrl = 'https://github.com/meth-browser/app'
+
+export default () => (
+  <div>
+    <p>Meth is a cross-platform wallet and Dapp browser that is under active development!</p>
+    <p>You can follow its progress at <a href={githubRepoUrl}>{githubRepoUrl}</a></p>
+  </div>
+)
